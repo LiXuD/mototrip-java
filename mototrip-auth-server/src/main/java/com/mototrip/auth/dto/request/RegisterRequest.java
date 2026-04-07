716f89a458e417f1140b2089a89746577ad5d1b1
lixd <xudong.li@hismartinfo.com>
2026-04-07 12:04:43 +0800

    fix: 为 DTO 类添加 Swagger @Schema 注解，完善接口文档参数说明

--- mototrip-auth-server/src/main/java/com/mototrip/auth/dto/request/RegisterRequest.java
+++ mototrip-auth-server/src/main/java/com/mototrip/auth/dto/request/RegisterRequest.java
@@ -1,18 +1,23 @@
 package com.mototrip.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(description = "注册请求")
 public class RegisterRequest {
+    @Schema(description = "用户名")
     @NotBlank(message = "用户名不能为空")
     private String username;
 
+    @Schema(description = "密码")
     @NotBlank(message = "密码不能为空")
     @Size(min = 8, message = "密码长度至少8位")
     private String password;
 
+    @Schema(description = "昵称")
     @NotBlank(message = "昵称不能为空")
     private String nickname;
 }
--- mototrip-auth-server/src/main/java/com/mototrip/auth/dto/request/WechatLoginRequest.java
+++ mototrip-auth-server/src/main/java/com/mototrip/auth/dto/request/WechatLoginRequest.java
@@ -1,10 +1,13 @@
 package com.mototrip.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@Schema(description = "微信登录请求")
 public class WechatLoginRequest {
+    @Schema(description = "微信登录code")
     @NotBlank(message = "code不能为空")
     private String code;
 }
