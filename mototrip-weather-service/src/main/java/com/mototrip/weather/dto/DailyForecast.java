077e917922077156d349c066a727d1a8a69df7c2
lixd <xudong.li@hismartinfo.com>
2026-04-08 09:03:02 +0800

    fix: 为 weather 服务的 DailyForecast 添加 Swagger @Schema 注解

--- mototrip-weather-service/src/main/java/com/mototrip/weather/dto/DailyForecast.java
+++ mototrip-weather-service/src/main/java/com/mototrip/weather/dto/DailyForecast.java
@@ -1,5 +1,6 @@
 package com.mototrip.weather.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,13 +12,29 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "每日天气预报")
 public class DailyForecast {
+    @Schema(description = "日期")
     private String date;
+    
+    @Schema(description = "最高温度")
     private double maxTemp;
+    
+    @Schema(description = "最低温度")
     private double minTemp;
+    
+    @Schema(description = "天气代码")
     private String weatherCode;
+    
+    @Schema(description = "天气描述")
     private String weatherDescription;
+    
+    @Schema(description = "天气图标")
     private String icon;
+    
+    @Schema(description = "降水概率")
     private double precipitationProbability;
+    
+    @Schema(description = "最大风速")
     private double maxWindSpeed;
 }
